4ce3f693db0ece4641e6295373b7586dbfe0fb76
dhershman1 <dustinh17@gmail.com>
2018-03-20 20:20:29 -0400

    Test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@
 'use strict';
 
  var debounce = function(func, wait, immediate) {
-    var timeout;
+     var timeout;
+
+     console.log('Thing');
+
     return function() {
         var context = this,
             args = arguments;
